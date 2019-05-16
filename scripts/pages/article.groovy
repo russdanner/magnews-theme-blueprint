2dd77a83682fc2f41fa6e5e20c97574a7f518f69
admin admin <evaladmin@example.com>
2019-05-16 16:05:00 -0400

    User admin wrote content /scripts/pages/article.groovy

--- scripts/pages/article.groovy
+++ scripts/pages/article.groovy
@@ -1,2 +1,6 @@
 def defaultArticle = siteItemService.getSiteItem("/site/articles/2019/05/a-better-way-to-artichokes.xml")
-templateModel.currentArticle = (params.id) ? siteItemService.getSiteItem("/site/articles/"+params.id+".xml") : defaultArticle
+templateModel.currentArticle  = defaultArticle
+
+if(params.id) {
+    templateModel.currentArticle = siteItemService.getSiteItem("/site/articles/"+params.id+".xml")
+}
